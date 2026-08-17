a843929565f5b2d3ce69ebc06d219da1a89b960c
Vic Genin <aie0.com@gmail.com>
2026-08-17 10:05:34 +0300

    build: prep windows arm zig 0.17

--- build.zig
+++ build.zig
@@ -428,7 +428,9 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
-    if (b.args) |argv| run_exe.addArgs(argv);
+    if (@hasField(std.Build, "args")) {
+        if (b.args) |argv| run_exe.addArgs(argv);
+    }
     const run_step = b.step("run", "Run tickoni-supervisor");
     run_step.dependOn(&run_exe.step);
 
@@ -1887,7 +1889,9 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(cli_exe);
 
     const run_cli = b.addRunArtifact(cli_exe);
-    if (b.args) |argv| run_cli.addArgs(argv);
+    if (@hasField(std.Build, "args")) {
+        if (b.args) |argv| run_cli.addArgs(argv);
+    }
     const run_cli_step = b.step("run-cli", "Run tickoni demo CLI");
     run_cli_step.dependOn(&run_cli.step);
 
