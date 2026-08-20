d9b09412b540b0e297e8115d782c207ddbf47e7e
Vic Genin <aie0.com@gmail.com>
2026-08-20 13:15:14 +0300

    build: remove obsolete lib and strategy modules

--- tickoni-build/lib.zig
+++
@@ -1,7 +0,0 @@
-/// Re-export entry point for all build/lib/ modules.
-
-pub const shims = @import("lib/shims.zig");
-pub const codec = @import("lib/codec.zig");
-pub const firedancer_shims = @import("lib/firedancer_shims.zig");
-pub const topo_run = @import("lib/topo_run.zig");
-pub const tile_run = @import("lib/tile_run.zig");
