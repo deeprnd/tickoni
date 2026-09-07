e15b33e4d0aa8eaed86ea44ca4db18e37dd62783
Vic Genin <aie0.com@gmail.com>
2026-09-08 01:10:53 +0300

    fix(waltz/http): reorder includes for INADDR_LOOPBACK on macOS

--- src/waltz/http/test_http_server.c
+++ src/waltz/http/test_http_server.c
@@ -4,10 +4,10 @@
 
 #include <arpa/inet.h>
 #include <errno.h>
-#include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
 
 struct overflow_close_state {
